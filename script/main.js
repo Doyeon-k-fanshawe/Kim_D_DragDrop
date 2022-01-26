c5b917aebdd6331c7d2097c3d788b030dd2428a4
Doyeon <d_kim120803@fanshaweonline.ca>
2022-01-26 14:41:48 -0500

    adding some parts

--- script/main.js
+++ script/main.js
@@ -1,4 +1,34 @@
 (() => {
-	// set up the puzzle pieces and boards
-	
+	// make the connections to the elements on the page
+	// that we want the user to interact with
+	const theButtons = document.querySelectorAll("#buttonHolder img"),
+				theGameBoard = document.querySelector(".puzzle-board");
+
+	// theButtons becomes this:
+	// [
+	// <img>
+	// <img>
+	// <img>
+	// <img>
+	// ]
+	//
+
+	function changeBgImg() {
+		// debugger; //pause our code execution at this point
+		// way 1 ------------------------
+		// let key = this.dataset.bgref;
+		// console.log(key);
+
+		// theGameBoard.style.backgroundImage = `url(images/backGround${key}.jpg)`;
+		// ------------------------------------------------------------------------- //
+		// way 2 -----------------------------------------------------------------------------//
+		theGameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`;
+
+		// `` => this is a javascript templete string. You can use it to wite a bit of inline javascript with will be interpreted at runtime
+		// search for MDN Javascript Templete String
+	}
+
+	// these are the "triggers" we want the user to use to fire  off events
+	theButtons.forEach(button => button.addEventListener("click", changeBgImg));
+
 })();
